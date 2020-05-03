636868df751ce346b18137f53c44686687f6c797
Mandeep Sidhu <mandeep.sidhu2@gmail.com>
2020-05-03 15:04:58 +0530

    added table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { MatTableModule } from '@angular/material/table'  
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatTableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/homepage/homepage.component.ts
@@ -0,0 +1,114 @@
+import { Component, OnInit } from '@angular/core';
+import {animate, state, style, transition, trigger} from '@angular/animations';
+@Component({
+  selector: 'app-homepage',
+  templateUrl: './homepage.component.html',
+  styleUrls: ['./homepage.component.css'],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({height: '0px', minHeight: '0'})),
+      state('expanded', style({height: '*'})),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
+})
+export class HomepageComponent implements OnInit {
+  dataSource = ELEMENT_DATA;
+  columnsToDisplay = ['name', 'easy', 'medium', 'hard'];
+  expandedElement: PeriodicElement | null;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  
+}
+
+export interface PeriodicElement {
+  name: string;
+  hard: number;
+  easy: number;
+  medium: number;
+  description: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {
+    hard: 1,
+    name: 'Hydrogen',
+    easy: 1.0079,
+    medium: 2,
+    description: `Hydrogen is a chemical element with medium H and atomic number 1. With a standard
+        atomic easy of 1.008, hydrogen is the lightest element on the periodic table.`
+  }, {
+    hard: 2,
+    name: 'Helium',
+    easy: 4.0026,
+    medium: 4,
+    description: `Helium is a chemical element with medium He and atomic number 2. It is a
+        colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
+        group in the periodic table. Its boiling point is the lowest among all the elements.`
+  }, {
+    hard: 3,
+    name: 'Lithium',
+    easy: 6.941,
+    medium: 5,
+    description: `Lithium is a chemical element with medium Li and atomic number 3. It is a soft,
+        silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
+        lightest solid element.`
+  }, {
+    hard: 4,
+    name: 'Beryllium',
+    easy: 9.0122,
+    medium: 6,
+    description: `Beryllium is a chemical element with medium Be and atomic number 4. It is a
+        relatively rare element in the universe, usually occurring as a product of the spallation of
+        larger atomic nuclei that have collided with cosmic rays.`
+  }, {
+    hard: 5,
+    name: 'Boron',
+    easy: 10.811,
+    medium: 2,
+    description: `Boron is a chemical element with medium B and atomic number 5. Produced entirely
+        by cosmic ray spallation and supernovae and not by stellar nucleosynthesis, it is a
+        low-abundance element in the Solar system and in the Earth's crust.`
+  }, {
+    hard: 6,
+    name: 'Carbon',
+    easy: 12.0107,
+    medium: 9,
+    description: `Carbon is a chemical element with medium C and atomic number 6. It is nonmetallic
+        and tetravalent—making four electrons available to form covalent chemical bonds. It belongs
+        to group 14 of the periodic table.`
+  }, {
+    hard: 7,
+    name: 'Nitrogen',
+    easy: 14.0067,
+    medium: 1,
+    description: `Nitrogen is a chemical element with medium N and atomic number 7. It was first
+        discovered and isolated by Scottish physician Daniel Rutherford in 1772.`
+  }, {
+    hard: 8,
+    name: 'Oxygen',
+    easy: 15.9994,
+    medium: 4,
+    description: `Oxygen is a chemical element with medium O and atomic number 8. It is a member of
+         the chalcogen group on the periodic table, a highly reactive nonmetal, and an oxidizing
+         agent that readily forms oxides with most elements as well as with other compounds.`
+  }, {
+    hard: 9,
+    name: 'Fluorine',
+    easy: 18.9984,
+    medium: 6,
+    description: `Fluorine is a chemical element with medium F and atomic number 9. It is the
+        lightest halogen and exists as a highly toxic pale yellow diatomic gas at standard
+        conditions.`
+  }, {
+    hard: 10,
+    name: 'Neon',
+    easy: 20.1797,
+    medium: 9,
+    description: `Neon is a chemical element with medium Ne and atomic number 10. It is a noble gas.
+        Neon is a colorless, odorless, inert monatomic gas under standard conditions, with about
+        two-thirds the density of air.`
+  },
+];
